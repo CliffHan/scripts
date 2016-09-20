44088cf41a05b411bbf9ff6a5e93900b002464fa
hanp <hanp@tcl.com>
2016-09-20 11:15:05 +0800

    init import

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# scripts
+# 这里是一些个人脚本集合
+
+> * parse_bing_today_image.sh
+> > 解析bing今日图片，输出图片链接，Linux平台，需要sed/grep
+
---
+++ parse_bing_today_image.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+### this script parse bing today image url from bing json ###
+
+function jsonval {
+    temp=`echo $json | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w $prop`
+    echo ${temp##*|}
+}
+
+bing_img_json_url="http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1"
+json=`curl -s $bing_img_json_url`
+prop='url'
+picurl=`jsonval`
+
+echo $picurl
+
+### download it if you want
+#`curl -s -X GET $picurl -o $1.jpg`
+
